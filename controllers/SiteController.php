fd30b774d0fcbcb998f4ffa0669e7e16039b4862
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-10-21 17:39:17 +0600

    Display list of teams

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\Conference;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
@@ -61,7 +62,11 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $conferences = Conference::find()
+            ->with('divisions.teams')
+            ->all();
+
+        return $this->render('index', compact('conferences'));
     }
 
     /**
