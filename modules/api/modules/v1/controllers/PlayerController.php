43550a7fe3992a963195fd73659c520208e8e119
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-10-24 16:26:09 +0600

    OpenAPI: annotate index action of the player controller

--- modules/api/modules/v1/controllers/PlayerController.php
+++ modules/api/modules/v1/controllers/PlayerController.php
@@ -5,6 +5,56 @@ namespace app\modules\api\modules\v1\controllers;
 use yii\rest\ActiveController;
 use app\modules\api\modules\v1\models\Player;
 
+/**
+ * @OA\Get(
+ *     path="/player",
+ *     @OA\Parameter(
+ *         in="query",
+ *         name="_format",
+ *         required=false,
+ *         @OA\Schema(
+ *             type="string",
+ *             oneOf={"xml","json"}
+ *         )
+ *     ),
+ *     @OA\Parameter(
+ *         in="query",
+ *         name="page",
+ *         required=false,
+ *         @OA\Schema(
+ *             type="int",
+ *             format="int64"
+ *         )
+ *     ),
+ *     @OA\Parameter(
+ *         in="query",
+ *         name="sort",
+ *         required=false,
+ *         examples={"&sort=-id","&sort=+name"},
+ *         @OA\Schema(
+ *             type="string"
+ *         )
+ *     ),
+ *     @OA\Parameter(
+ *         in="query",
+ *         name="expand",
+ *         description="Expands relationships",
+ *         required=false,
+ *         example="&expand=team",
+ *         @OA\Schema(
+ *             type="string"
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="200",
+ *         description="Paginated list of the players"
+ *     ),
+ *     @OA\Response(
+ *         response="default",
+ *         description="An unexpected error"
+ *     )
+ * )
+ */
 class PlayerController extends ActiveController
 {
     /** @var string */
