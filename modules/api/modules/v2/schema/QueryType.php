d044fb0f9fab525cf7efcf30359a6036c021ef1e
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-10-29 20:28:19 +0600

    GraphQL: pagination example

--- modules/api/modules/v2/schema/QueryType.php
+++ modules/api/modules/v2/schema/QueryType.php
@@ -4,6 +4,7 @@ namespace app\modules\api\modules\v2\schema;
 
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
+use yii\data\Pagination;
 use app\models\Player;
 use app\models\Team;
 
@@ -19,7 +20,25 @@ class QueryType extends ObjectType
                 return [
                     'players' => [
                         'type' => Type::listOf(Types::player()),
-                        'resolve' => fn (): array => Player::find()->all()
+                        'args' => [
+                            'page' => [
+                                'type' => Type::int(),
+                                'defaultValue' => 1
+                            ]
+                        ],
+                        'resolve' => function ($root, $args) {
+                            $query = Player::find();
+
+                            $pagination = new Pagination([
+                                'totalCount' => (clone $query)->count()
+                            ]);
+
+                            $pagination->setPage($args['page'] - 1, true);
+
+                            return $query->offset($pagination->offset)
+                                ->limit($pagination->limit)
+                                ->all();
+                        }
                     ],
                     'player' => [
                         'type' => Types::player(),
